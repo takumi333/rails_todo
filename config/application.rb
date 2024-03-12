db065c844b1d8af84b57923f9c9f18184d202b72
takumi333 <takumiatat@yahoo.co.jp>
2024-03-12 21:13:23 +0900

    コントロールファイル作成完了

--- config/application.rb
+++ config/application.rb
@@ -6,11 +6,23 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module VerificationApp
+module Rails_todo_App
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
+    # generateファイルの設定
+    config.generators do |g|
+      g.skip_routes     true           # routes.rbにルーティングを追加しない、falseで追加する(falseがデフォルト)
+      g.assets          false          # assets以下のファイル(CSS, JavaScriptファイル)を作成しない 
+      
+      # g.assetsは分けて記述することもできる
+      # g.stylesheets   false          # stylesheets以下にファイルを作成しない  
+      # g.javascripts   false          # javascripts以下にファイルを作成しない
+
+      g.helper          false          # helper以下にファイルを作成しない 
+      g.test_framework  false          # test以下にファイル作成しない     
+    end
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
